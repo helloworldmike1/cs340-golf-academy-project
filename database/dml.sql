edc970bc0b0cb9072cb43d0942ae7a5089cd2caa
Richard Hong <rhong24@gmail.com>
2026-05-06 15:19:32 -0500

    improving legibility and case sensitivity of operations on dml.sql

--- database/dml.sql
+++ database/dml.sql
@@ -1,51 +1,46 @@
 
 -- get all Customers query 
-Select C.customerID,  C.firstName, C.lastName, C.email, C.phone, C. membershipId 
-from Customers ;
+SELECT C.customerID,  C.firstName, C.lastName, C.email, C.phone, C. membershipId FROM Customers;
 
 -- query to get customer membership name drop down
-select membershipId,,name from Memberships;
-
+SELECT membershipId, name FROM Memberships;
 
 -- get memberships query
-select membershipId, name, price, offering from Memberships;
-
+SELECT membershipId, name, price, offering FROM Memberships;
 
 -- get all instructors query
 
-select instructorId, firstName,lastName, email, phone, bio 
-from Instructors ;
+SELECT instructorId, firstName,lastName, email, phone, bio FROM Instructors;
 
 -- get all Bays query
-select bayId, name, handedness, activefrom Bays; 
+SELECT bayId, name, handedness, active FROM Bays; 
 
 -- select from lessons
-select lessonId, lessonTime, duration, instructorId, bayId from Lessons;
+SELECT lessonId, lessonTime, duration, instructorId, bayId FROM Lessons;
 
 -- select from Lesson Participant 
-
-select lessonId, customerId  from LessonParticipants;
-
+SELECT lessonId, customerId FROM LessonParticipants;
 
 -- Update to Bays 
-update Bays 
-
-set name=:update_bay_name, 
+UPDATE Bays 
+SET NAME=:update_bay_name, 
     handedness = :update_bay_handedness 
     active = :update_bay_active
-
-    where bayId = :update_bay_id;
-
+    WHERE bayId = :update_bay_id;
 
 -- Delete a bay
-
-Delete from Bays where BayId =delete_bay_id;
-
+Delete FROM Bays WHERE BayId = delete_bay_id;
 
 -- Create New a new customer in customers table
 -- membershipId will be from drop down
-Insert Into Customers (firstName, lastName, email, phone, membershipId) values 
-( :create_customer_firstName, :create_customer_lastName, :create_customer_email, :create_customer_phone, :membershipId_from_create_customer_membership_dropdown
+INSERT INTO Customers (firstName, lastName, email, phone, membershipId) 
+VALUES 
+( 
+    :create_customer_firstName, 
+    :create_customer_lastName, 
+    :create_customer_email, 
+    :create_customer_phone, 
+    :membershipId_from_create_customer_membership_dropdown
 );
 
 
